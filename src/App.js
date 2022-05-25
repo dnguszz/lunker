2f3687d3c57b2d86e4bc1398612be3d05842dabf
dnguszz <pinkfloyd951@naver.com>
2022-05-25 17:45:23 +0900

    리액트 프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          런커의 첫 화면입니다.
         </p>
         <a
           className="App-link"
